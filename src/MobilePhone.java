1568ee0347690b6bc72e7be80a74f5fc3022a2fb
mandeep sidhu <sidhumandeep@gmail.com>
2024-04-04 20:43:39 -0400

    myfirstclass

---
+++ src/MobilePhone.java
@@ -0,0 +1,28 @@
+package   firstclassjava;
+
+public class MobilePhone {
+
+          // DataType
+	   
+	      //DataType  variableName = value
+           String  colour  = "White";
+
+           //byte, int ,long, float,double,
+          int  memory = 8;
+          
+          double screensize = 6.5;
+          
+          String material ="Metal";
+          
+          float cameraConfiguration = 1.3f ;
+          
+         boolean  isFaceRecongAvailable = true;
+         
+         String brandName = "IPHONE";
+         
+         //Method or Function
+         void makeCalls() {
+        	 //print a line
+         }
+        }
+
